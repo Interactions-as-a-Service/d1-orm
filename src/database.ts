4d378ce69b45f19ffe5e76727c3cd7be924a8bbf
Skye <46286597+Skye-31@users.noreply.github.com>
2022-08-30 17:16:26 +0100

    Ensure ORM implements database

--- src/database.ts
+++ src/database.ts
@@ -1,11 +1,11 @@
-export class D1Orm {
+export class D1Orm implements D1Database {
 	database: D1Database;
 
 	constructor(database: D1Database) {
 		this.database = database;
 	}
 
-	async exec<T = unknown>(query: string): Promise<D1Result<T>> {
+	async exec<T>(query: string): Promise<D1Result<T>> {
 		return this.database.exec<T>(query);
 	}
 
@@ -13,13 +13,11 @@ export class D1Orm {
 		return this.database.dump();
 	}
 
-	async prepare(query: string): Promise<D1PreparedStatement> {
+	prepare(query: string): D1PreparedStatement {
 		return this.database.prepare(query);
 	}
 
-	async batch<T = unknown>(
-		statements: D1PreparedStatement[]
-	): Promise<D1Result<T>[]> {
+	async batch<T>(statements: D1PreparedStatement[]): Promise<D1Result<T>[]> {
 		return this.database.batch<T>(statements);
 	}
 }
