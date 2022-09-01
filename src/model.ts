a20383fab102e406a6c1e72f3f747fbd6736d069
Skye <46286597+Skye-31@users.noreply.github.com>
2022-09-01 11:34:57 +0100

    Update method

--- src/model.ts
+++ src/model.ts
@@ -3,11 +3,11 @@ import { DataTypes } from "./datatypes";
 
 export class Model<T> {
 	constructor(options: ModelOptions, columns: ModelColumns) {
-		this.D1Orm = options.D1Orm;
+		this.#D1Orm = options.D1Orm;
 		this.tableName = options.tableName;
 		this.columns = columns;
 
-		if (!(this.D1Orm instanceof D1Orm)) {
+		if (!(this.#D1Orm instanceof D1Orm)) {
 			throw new Error("Options.D1Orm is not an instance of D1Orm");
 		}
 		if (!this.tableName?.length) {
@@ -37,7 +37,7 @@ export class Model<T> {
 	}
 	public tableName: string;
 	public readonly columns: ModelColumns;
-	private readonly D1Orm: D1Orm;
+	readonly #D1Orm: D1Orm;
 
 	public async CreateTable(
 		{ strategy }: CreateTableOptions = { strategy: "default" }
@@ -71,32 +71,32 @@ export class Model<T> {
 		if (strategy === "force") {
 			await this.DropTable(true);
 		}
-		return this.D1Orm.exec(statement);
+		return this.#D1Orm.exec(statement);
 	}
 
 	public async DropTable(silent?: boolean): Promise<D1Result<unknown>> {
 		if (silent) {
-			return this.D1Orm.exec(`DROP TABLE IF EXISTS ${this.tableName};`);
+			return this.#D1Orm.exec(`DROP TABLE IF EXISTS ${this.tableName};`);
 		}
-		return this.D1Orm.exec(`DROP TABLE ${this.tableName};`);
+		return this.#D1Orm.exec(`DROP TABLE ${this.tableName};`);
 	}
 
-	private statementAddBindings(
+	#statementAddBindings(
 		query: string,
 		data: Record<string, unknown>
 	): D1PreparedStatement {
-		const statement = this.D1Orm.prepare(query).bind(...Object.values(data));
+		const statement = this.#D1Orm.prepare(query).bind(...Object.values(data));
 		return statement;
 	}
 
-	private createInsertStatement(data: Partial<T>): D1PreparedStatement {
+	#createInsertStatement(data: Partial<T>): D1PreparedStatement {
 		const dataRecord = data as Record<string, unknown>;
 		const columnNames = Object.keys(dataRecord);
 		const columnSize = columnNames.length;
 		if (columnSize === 0) {
 			throw new Error("InsertOne called with no columns");
 		}
-		return this.statementAddBindings(
+		return this.#statementAddBindings(
 			`INSERT INTO ${this.tableName} (${columnNames.join(
 				", "
 			)}) VALUES (${"?, ".repeat(columnSize - 1)}?) RETURNING *;`,
@@ -105,15 +105,15 @@ export class Model<T> {
 	}
 
 	public async InsertOne(data: Partial<T>): Promise<D1Result<T>> {
-		return this.createInsertStatement(data).first<D1Result<T>>();
+		return this.#createInsertStatement(data).first<D1Result<T>>();
 	}
 
 	public async InsertMany(data: Partial<T>[]): Promise<D1Result<T>[]> {
 		const stmts: D1PreparedStatement[] = [];
 		for (const row of data) {
-			stmts.push(this.createInsertStatement(row));
+			stmts.push(this.#createInsertStatement(row));
 		}
-		return this.D1Orm.batch<T>(stmts);
+		return this.#D1Orm.batch<T>(stmts);
 	}
 
 	public async First(options: {
@@ -122,11 +122,11 @@ export class Model<T> {
 		const { where } = options;
 		const objectKeys = Object.keys(where as Record<string, unknown>);
 		if (objectKeys.length === 0) {
-			return this.D1Orm.prepare(
-				`SELECT * FROM ${this.tableName} LIMIT 1;`
-			).first<D1Result<T>>();
+			return this.#D1Orm
+				.prepare(`SELECT * FROM ${this.tableName} LIMIT 1;`)
+				.first<D1Result<T>>();
 		}
-		const stmt = this.statementAddBindings(
+		const stmt = this.#statementAddBindings(
 			`SELECT * FROM ${this.tableName} WHERE ` +
 				objectKeys.map((key) => `${key} = ?`).join(" AND ") +
 				" LIMIT 1;",
@@ -142,11 +142,13 @@ export class Model<T> {
 		const { where, limit } = options;
 		const objectKeys = Object.keys(where as Record<string, unknown>);
 		if (objectKeys.length === 0) {
-			return this.D1Orm.prepare(
-				`SELECT * FROM ${this.tableName}${limit ? `LIMIT ${limit}` : ""};`
-			).all<T>();
+			return this.#D1Orm
+				.prepare(
+					`SELECT * FROM ${this.tableName}${limit ? `LIMIT ${limit}` : ""};`
+				)
+				.all<T>();
 		}
-		const stmt = this.statementAddBindings(
+		const stmt = this.#statementAddBindings(
 			`SELECT * FROM ${this.tableName} WHERE ` +
 				objectKeys.map((key) => `${key} = ?`).join(" AND ") +
 				(limit ? ` LIMIT ${limit}` : ""),
@@ -162,15 +164,53 @@ export class Model<T> {
 		const { where, limit } = options;
 		const objectKeys = Object.keys(where as Record<string, unknown>);
 		if (objectKeys.length === 0) {
-			return this.D1Orm.prepare(
-				`DELETE FROM ${this.tableName}${limit ? `LIMIT ${limit}` : ""};`
-			).run();
+			return this.#D1Orm
+				.prepare(
+					`DELETE FROM ${this.tableName}${limit ? `LIMIT ${limit}` : ""};`
+				)
+				.run();
 		}
-		const stmt = this.statementAddBindings(
+		const stmt = this.#statementAddBindings(
 			`DELETE FROM ${this.tableName} WHERE ` +
 				objectKeys.map((key) => `${key} = ?`).join(" AND ") +
 				(limit ? ` LIMIT ${limit}` : ""),
 			where
+		);
+		return stmt.run();
+	}
+
+	public async Update(options: {
+		where: WhereOptions<T>;
+		data: Partial<T>;
+		limit?: number;
+	}): Promise<D1Result<unknown>> {
+		const { where, data } = options;
+		const dataKeys = Object.keys(data as Record<string, unknown>);
+		const whereKeys = Object.keys(where as Record<string, unknown>);
+		if (dataKeys.length === 0) {
+			throw new Error("Update called with no data");
+		}
+		if (whereKeys.length === 0) {
+			return this.#D1Orm
+				.prepare(
+					`UPDATE ${this.tableName} SET ${Object.keys(data)
+						.map((key) => `${key} = ?`)
+						.join(", ")}`
+				)
+				.bind(...Object.values(data))
+				.run();
+		}
+		const stmtArray = [...Object.values(data), ...Object.values(where)];
+		const params: Record<number, unknown> = {};
+		for (let i = 0; i < stmtArray.length; i++) {
+			params[i] = stmtArray[i];
+		}
+		const stmt = this.#statementAddBindings(
+			`UPDATE ${this.tableName} SET ${Object.keys(data)
+				.map((key) => `${key} = ?`)
+				.join(", ")} WHERE ` +
+				whereKeys.map((key) => `${key} = ?`).join(" AND "),
+			params
 		);
 		return stmt.run();
 	}
